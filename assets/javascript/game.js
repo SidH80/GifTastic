68f19eedc85cd7a8d4c6bcfdbac05b5f5a7c6946
lethalSid <senien.hicks@gmail.com>
2019-09-16 00:49:53 -0400

    reverted changes from previous moes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,43 +14,40 @@ function renderGiphy() {
   }).then(function(response) {
 
     console.log(JSON.stringify(response))
-    //data dump works
-    //$("#giphy-view").text(JSON.stringify(response));
+
     var results = response.data;
 
-          $("#giphy-view").empty();
-          // Looping over every result item
-          for (var i = 0; i < results.length; i++) {
+    $("#giphy-view").empty();
+    // Looping over every result item
+    for (var i = 0; i < results.length; i++) {
 
-            // Only taking action if the photo has an appropriate rating
-            if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
-              // Creating a div for the gif
-              var gifDiv = $("<div>");
+      // Only taking action if the photo has an appropriate rating
+      if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+        // Creating a div for the gif
+        var gifDiv = $("<div>");
 
-              // Storing the result item's rating
-              var rating = results[i].rating;
+        // Storing the result item's rating
+        var rating = results[i].rating;
 
-              // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + rating);
+        // Creating a paragraph tag with the result item's rating
+        var p = $("<p>").text("Rating: " + rating);
 
-              // Creating an image tag
-              var giphyImage = $("<img>");
+        // Creating an image tag
+        var giphyImage = $("<img>");
 
-              giphyImage.addClass("giphy");
+        giphyImage.addClass("giphy");
 
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              giphyImage.attr("src", results[i].images.fixed_height.url);
+        // Giving the image tag an src attribute of a proprty pulled off the
+        // result item
+        giphyImage.attr("src", results[i].images.fixed_height.url);
 
-              gifDiv.append(p);
-              gifDiv.append(giphyImage);
+        gifDiv.append(p);
+        gifDiv.append(giphyImage);
 
-              // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-              $("#giphy-view").prepend(gifDiv);
-
-
-          }
-        }
+        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+        $("#giphy-view").prepend(gifDiv);
+      }
+    }
 
   });
 
@@ -68,7 +65,7 @@ function renderButtons() {
 
   for(let i = 0; i < buttons.length; i++)
   {
-    //creates the button and sets it to a vairable
+    //creates the button and sets it to a variable
     btn = $("<button>");
 
     //names the button text the name pushed to the array
@@ -86,25 +83,17 @@ function renderButtons() {
 
 }
 
-// This function handles events where the add movie button is clicked
-
 $("#add-giphy").on("click", function (event) {
 
   event.preventDefault();
 
-  // Write code to grab the text the user types into the input field
-
   var button = $("#giphy-input").val().trim();
-  //.trim() gets rid of extra whitespace in front and behind
-  // Write code to add the new giphy into the buttons array
 
   buttons.push(button);
-  // The renderButtons function is called, rendering the list of giphy buttons
 
   renderButtons();
 });
 
 $(document).on("click", ".giphy-render", renderGiphy);
 
-// Calling the renderButtons function to display the initial list of movies
 renderButtons();
